4a69b01bfe308e94cd28d36a120f2a9926043dc3
MarsClaim <Luivil777@gmail.com>
2022-03-26 16:01:43 -0600

    fixing imports

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort
 from flask_cors import CORS
-import db_config as database
+import app.db_config as database
 
 app = Flask(__name__)
 api = Api(app)
